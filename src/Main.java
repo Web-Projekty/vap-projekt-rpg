b2ba319eaf60aeb04bfb0afe2ce07889b3272319
florixak <florixakcz@seznam.cz>
2024-02-13 22:41:11 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("RPG Game");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
