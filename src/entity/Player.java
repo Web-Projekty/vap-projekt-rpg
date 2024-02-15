913a893dd58392d161a0b0492061bd7c290177b0
Wlczak <adivlcak@seznam.cz>
2024-02-15 16:54:53 +0100

    Partial sprites + kinda laggy

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp;
@@ -16,31 +19,72 @@ public class Player extends Entity {
         setDefaultValues();
     }
 
+    public void GetPlayerImage() {
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
 
     public void update() {
         if (keyH.upPressed) {
             y -= speed;
+            direction = "up";
         }
         if (keyH.downPressed) {
             y += speed;
+            direction = "down";
         }
         if (keyH.leftPressed) {
             x -= speed;
+            direction = "left";
         }
         if (keyH.rightPressed) {
             x += speed;
+            direction = "right";
         }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
+//        g2.setColor(Color.white);
+//
+//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
 
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        GetPlayerImage();
+
+        switch (direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -84,7 +84,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
-player.draw(g2);
+        player.draw(g2);
         g2.dispose();
     }
 }
